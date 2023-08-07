1da610ca4355c6717f79392d9b6d4229fffe8d7e
Curtis Rueden <ctrueden@wisc.edu>
2023-08-06 19:30:42 -0500

    WIP19

--- bin/thicket.py
+++ bin/thicket.py
@@ -28,17 +28,17 @@ xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/x
 # do
 # grep "^$l.\{4\}" google-10000-english-usa-no-swears.txt | grep -v '.\{8\}' | shuf | head -n1
 # done
-NAMES = {
+NAMES = [
     "active", "bullet", "coral", "detail", "essence", "fonts", "games",
     "heating", "ignore", "journal", "knives", "lodge", "major", "neutral",
     "optics", "permits", "quoted", "rotary", "socket", "tickets", "upload",
     "vendors", "weight", "xhtml", "younger", "zoning",
-}
+]
 
 GROUP_ID = "org.scijava.jgo.thicket"
-ANCESTOR_COUNT = 5
+ANCESTOR_COUNT = 4
 MAX_IMPORTS = 3
-MAX_DEPTH = 10
+MAX_DEPTH = 8
 
 
 # -- Variables --
@@ -64,7 +64,9 @@ def random_version() -> str:
 
 
 def generate_pom(name, version=None, ancestor_count=ANCESTOR_COUNT, depth=0):
-    root = etree.fromstring(TEMPLATE)
+    # NB: The remove_blank_text=True option is needed to pretty-print the XML output:
+    # https://lxml.de/FAQ.html#why-doesn-t-the-pretty-print-option-reformat-my-xml-output
+    root = etree.fromstring(TEMPLATE, parser=etree.XMLParser(remove_blank_text=True))
     if ancestor_count > 0:
         parent = create_child(root, "parent")
         parent_name = f"{name}-parent{ancestor_count}"
@@ -83,24 +85,40 @@ def generate_pom(name, version=None, ancestor_count=ANCESTOR_COUNT, depth=0):
     bom_count = min(MAX_IMPORTS - depth, random.randint(0, MAX_IMPORTS))
     dep_mgmt = create_child(root, "dependencyManagement")
     dep_mgmt_deps = create_child(dep_mgmt, "dependencies")
-    for a in range(bom_count):
+    for i in range(bom_count):
         dep = create_child(dep_mgmt_deps, "dependency")
-        bom_name = f"{name}-bom{a + 1}"
+        bom_name = f"{name}-bom{i + 1}"
         create_child(dep, "groupId", GROUP_ID)
         create_child(dep, "artifactId", bom_name)
-        # FIXME: sometimes use properties... think about how best to randomize that
-        # - sometimes we want to define the property also in this POM, sometimes not.
-        # - sometimes we want to add an explicit version element override here, sometimes not.
+        # TODO: Sometimes use properties; sometimes allow the version to be managed.
         v = random_version()
         create_child(dep, "version", v)
         create_child(dep, "type", "pom")
         create_child(dep, "scope", "import")
         generate_pom(bom_name, version=v, ancestor_count=ancestor_count, depth=depth+1)
 
-    # START HERE - randomly add some managed dependencies here!
-    # randomize scope of these as well
-
-    # TODO: add dependencies
+    # Manage some dependencies.
+    properties = None
+    dep_count = random.randint(0, 5)
+    deps = set(random.choices(NAMES, k=dep_count))
+    for artifact_id in deps:
+        dep = create_child(dep_mgmt_deps, "dependency")
+        create_child(dep, "groupId", GROUP_ID)
+        create_child(dep, "artifactId", artifact_id)
+        # Sometimes use a property for the version, but other times not.
+        # - Sometimes we want to define the property also in this POM, sometimes not.
+        # - Sometimes we want to add an explicit version element override here, sometimes not.
+        # TODO: Sometimes allow the version to be managed.
+        v = random_version()
+        use_property = random.choice((True, False))
+        if use_property:
+            if properties is None:
+                properties = create_child(root, "properties")
+            prop_tag = artifact_id + ".version"
+            create_child(properties, prop_tag, v)
+            v = "${" + prop_tag + "}"
+        create_child(dep, "version", v)
+        # TODO: Consider randomizing classifier and/or scope of these as well.
 
     with open((OUTPUT_DIR / name).with_suffix(".pom"), "w") as f:
         xml = etree.tostring(root, xml_declaration=True, pretty_print=True, encoding="utf-8").decode()
